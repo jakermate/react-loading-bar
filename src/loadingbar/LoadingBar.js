21bef73f8ad719209e39700ed4023a62e50b3859
jm <root1@root.local>
2020-09-06 16:46:02 -0700

    Added more parameters and definitions.

--- src/loadingbar/LoadingBar.js
+++ src/loadingbar/LoadingBar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
-import PropTypes from 'prop-types'
-import earth from '../earthspin.mp4'
+import PropTypes from "prop-types"
+import earth from "../earthspin.mp4"
 
 /**
  * Usage:
@@ -13,11 +13,10 @@ import earth from '../earthspin.mp4'
  * ```
  */
 export default function LoadingBar(props) {
-
   // get URI for resource to load/download
   const URI = props.URI // this will need to throw error if not present
   if (!URI) {
-      console.log(URI)
+    console.log(URI)
     // catch missing prop error
     throw new Error(
       "URI for file to download is required. (URI prop in LoadingBar component.)"
@@ -45,49 +44,60 @@ export default function LoadingBar(props) {
   const [received, setReceived] = useState(0)
   const [complete, setComplete] = useState(false)
   // start fetch and create readable stream
-  useEffect(() => {get()}, []) // call upon component mount
-
+  useEffect(() => {
+    get()
+  }, []) // call upon component mount
 
   // second order functions passed in via props
   const onComplete = props.onComplete || null
 
-  function get(){
+  function get() {
     fetch(earth)
-    .then(res => {
-        let size = res.headers.get('Content-Length')
-        console.log('Total Size: '+ size)
+      .then((res) => {
+        let size = res.headers.get("Content-Length")
+        console.log("Total Size: " + size)
         setSize(size)
         return res.body
-    })
-    .then(body => {
+      })
+      .then((body) => {
         let reader = body.getReader()
         return new ReadableStream({
-            start(controller){
-                return pump()
-                function pump(){
-                    return reader.read().then(({done, value})=>{
-                        if(done){
-                            controller.close()
-                            return
-                        }
-                        controller.enqueue(value)
-                        let size = value.length
-                        setReceived(old => old + size)
-                        console.log(size)
-                        return pump()
-                    })
+          start(controller) {
+            return pump()
+            function pump() {
+              return reader.read().then(({ done, value }) => {
+                if (done) {
+                  controller.close()
+                  setComplete(true)
+                  return
                 }
+                controller.enqueue(value)
+                let size = value.length
+                setReceived((old) => old + size)
+                console.log(size)
+                return pump()
+              })
             }
+          },
         })
-    })
-    .then(stream => setComplete(true))
+      })
   }
-  // chunk read callback
-  useEffect(()=>{
-    console.log(received)
-  },[received])
 
-    //   render
+  // onComplete callback if provided by dev
+  useEffect(()=>{
+    // if stream is complete and onComplete callback is provided, run callback
+    if(complete && onComplete && typeof onComplete === 'function'){
+        onComplete()
+    }
+    return
+  }, [complete])
+
+  // chunk read callback
+  useEffect(() => {
+    console.log(received)
+  }, [received])
+
+  //   render
   return (
     // container element
     <div
@@ -108,7 +118,7 @@ export default function LoadingBar(props) {
         <div
           className="react-loading-bar-inner"
           style={{
-            width: `${(received/size)*100}%`,
+            width: `${(received / size) * 100}%`,
             height: "20px",
             background: "black",
           }}
@@ -129,17 +139,26 @@ export default function LoadingBar(props) {
   )
 }
 
-
 // propTypes
 LoadingBar.propTypes = {
-    /** {string} of path to asset location. */
-    URI: PropTypes.string,
-    /** {object} containing user defined option values. */
-    options: PropTypes.object,
-    /** {object} containing user defined styles in react inline-styles format. */
-    style: PropTypes.object,
-    /** {string} for use in list via array.map function */
-    key: PropTypes.string,
-    /** {function}: callback to fire (in parent component) on completion of download */
-    onComplete: PropTypes.func
+  /** {string} of path to asset location. */
+  URI: PropTypes.string,
+  /** {object} containing user defined styles in react inline-styles format. */
+  style: PropTypes.object,
+  /** {string} for use in list via array.map function */
+  key: PropTypes.string,
+  /** {function}: callback to fire (in parent component) on completion of download */
+  onComplete: PropTypes.func,
+  /** {string} message to display after download is complete */
+  doneMessage: PropTypes.string,
+  /** {boolean} modal determines if this behaves like a normally styled component, or if the loader will behave like a modal, taking up the whole pages focus */
+  modal: PropTypes.bool,
+  /** {boolean} display percentage complete in addition to progress bar */
+  displayPercent: PropTypes.bool,
+  /** {string} message to display while in progress */
+  hint: PropTypes.string,
+  /** {boolean} to show/hide hint message */
+  showHint: PropTypes.bool,
+  /** {string: low, medium, high} determines how smooth the animation of the progress bar is*/
+  smoothing: PropTypes.string
 }
