dbdf1494bfc904c86cbf2b4ac9d0241041c0481a
jm <root1@root.local>
2020-09-17 18:32:49 -0700

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,39 @@
-const path = require('path')
+const path = require("path")
 module.exports = {
-    "mode": "development",
-    "entry": "./src/loaders/ProgressBar.js",
-    "output": {
-        "path": path.resolve(__dirname, 'dist'),
-        "filename": 'index.js'
+  mode: "production",
+  entry: "./src/loaders/ProgressBar.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    // library: "react-progress-loaders",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {},
+        },
       },
-    "module": {
-        "rules": [
-            {
-                "test": /\.(js|jsx)$/,
-                "exclude": /node_modules/,
-                "use": {
-                    "loader": "babel-loader",
-                    "options": {
-                    }
-                }
-            }
-        ]
-    }
+    ],
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    },
+  },
+  externals: [
+    {
+      'react': {
+        amd: 'react',
+        commonjs: 'react',
+        commonjs2: 'react',
+        root: 'React'
+      }
+    },
+    'react-dom'
+  ],
 }
